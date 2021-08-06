912fb53fdba600441e0d221865f905bfa6dd5d5d
unknown <faridun.nk@gmail.com>
2021-08-07 00:28:01 +0500

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson2App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            byte b = Convert.ToByte(Console.ReadLine());
+            if (b<15) Console.WriteLine("Промежуток [0 - 14]");
+            else if(b<36) Console.WriteLine("[15 - 35]");
+            else if(b<51) Console.WriteLine("[36 - 50]");
+            else if(b<101) Console.WriteLine("[50 - 100]");
+            else 
+            { 
+                Console.WriteLine("Этот число не входит ни в один из промежутков");
+            }
+        }
+    }
+}g
